86559ad0fdab1f2b483885771b189d57f56ca206
Manojkumarks02 <manojkumarks063@gmail.com>
2024-06-12 16:30:19 +0530

    initial commit

---
+++ src/main/java/com/jenkins/controller/DemoController.java
@@ -0,0 +1,16 @@
+package com.jenkins.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class DemoController {
+
+    @GetMapping("/get")
+    public String getMessage(){
+        return "hello docker String....!";
+    }
+}
